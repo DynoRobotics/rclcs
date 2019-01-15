700532a4a513c79e2c48cf4193e912b9ff373093
Samuel Lindgren <samuellindgren91@gmail.com>
2019-01-15 19:52:20 +0100

    Make context IDisposable

--- src/dotnet/Context.cs
+++ src/dotnet/Context.cs
@@ -7,15 +7,35 @@ namespace rclcs
 	/// <summary>
 	/// Represents a managed ROS context
 	/// </summary>
-	public class Context
+	public class Context: IDisposable
 	{
-        //TODO: Change to IDisposable?
+        //TODO: Init context when created?
 
         internal rcl_context_t handle;
+
+        private bool isInit;
+        private bool disposed;
 
         public Context()
         {
             handle = NativeMethods.rcl_get_zero_initialized_context();
+        }
+
+        public void Init()
+        {
+            rcl_init_options_t init_options = NativeMethods.rcl_get_zero_initialized_init_options();
+            rcl_allocator_t allocator = NativeMethods.rcl_get_default_allocator();
+
+            Utils.CheckReturnEnum(NativeMethods.rcl_init_options_init(ref init_options, allocator));
+            Utils.CheckReturnEnum(NativeMethods.rcl_init(0, null, ref init_options, ref handle));
+
+            isInit = true;
+        }
+
+        public void Shutdown()
+        {
+            Utils.CheckReturnEnum(NativeMethods.rcl_shutdown(ref handle));
+            isInit = false;
         }
 
         public bool Ok
@@ -23,10 +43,33 @@ namespace rclcs
             get { return NativeMethods.rcl_context_is_valid(ref handle); }
         }
 
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    // Dispose managed resources.
+                }
+
+                if(isInit)
+                {
+                    Shutdown();
+                }
+                NativeMethods.rcl_context_fini(ref handle);
+
+                disposed = true;
+            }
+        }
+
         ~Context()
         {
-            //TODO: figure out if this is correct...
-            NativeMethods.rcl_context_fini(ref handle);
+            Dispose(false);
         }
 
     }
--- src/dotnet/Rclcs.cs
+++ src/dotnet/Rclcs.cs
@@ -3,24 +3,18 @@ namespace rclcs
 {
     public static class Rclcs
     {
-        //TODO: Change context to IDisposable?
+        //TODO: Init context when created instaid?
 
         public static void Init(Context context)
         {
-            rcl_init_options_t init_options = NativeMethods.rcl_get_zero_initialized_init_options();
-            rcl_allocator_t allocator = NativeMethods.rcl_get_default_allocator();
-
-            Utils.CheckReturnEnum(NativeMethods.rcl_init_options_init(ref init_options, allocator));
-            Utils.CheckReturnEnum(NativeMethods.rcl_init(0, null, ref init_options, ref context.handle));
+            context.Init();
         }
     
 
         public static void Shutdown(Context context)
         {
-            Utils.CheckReturnEnum(NativeMethods.rcl_shutdown(ref context.handle));
-            Utils.CheckReturnEnum(NativeMethods.rcl_context_fini(ref context.handle));
+            context.Shutdown();
         }
-
 
         public static Node CreateNode(string nodeName, Context context, string nodeNamespace = null)
         {
